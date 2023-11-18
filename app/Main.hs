df1e6bfc17cd7109869f20072070a40f8b9bc43f
Khanh Hua <giakhanh2487@gmail.com>
2023-11-18 23:08:10 +0100

    Expose QuickSearch

--- app/Main.hs
+++ app/Main.hs
@@ -4,23 +4,25 @@ import Control.Class (Applet)
 import Control.HttpApplet (dispatcher, runApplet)
 import Control.HttpConnection (serve)
 import Control.Monad.Trans.Reader (asks, withReader)
+import Data.List (isPrefixOf)
 import Data.Response (htmlText, plainText)
 import Data.Router (get, post)
 
 import Parsers.UrlParser (root, s, top, var, (//))
 
-import Components.ToDoComponent (ToDoModel (ToDoModel), todoComponent, TemplateXComponent)
+import Components.QuickSearch (QSModel (Model), bind, quicksearch, searchHistory)
+import Components.ToDoComponent (TemplateXComponent, ToDoModel (ToDoModel), todoComponent)
 import Data.Builder (buildHtml)
 import Data.ByteString.Builder (toLazyByteString)
 import Data.ByteString.Lazy (toStrict)
-import Data.Maybe (fromMaybe, fromJust)
+import Data.Maybe (fromJust, fromMaybe)
 import Data.Request (Request (params), body)
 import qualified Data.Syntax as H
 import qualified Data.Tags as H
 import Parsers.FormDataParser (formData)
 import Parsers.Parser (parse)
-import qualified Template.ElementX as T
 import Template.Class (renderHtml)
+import qualified Template.ElementX as T
 
 main :: IO ()
 main = serve Nothing "8080" $ runApplet $ dispatcher routingTable
@@ -31,6 +33,8 @@ main = serve Nothing "8080" $ runApplet $ dispatcher routingTable
     , get (root // s "pages" // var ":id") pageN
     , get (root // s "todos") todoList
     , post (root // s "todos") todoList
+    , get (root // s "search") search
+    , post (root // s "search") search
     ]
 
 home :: Applet
@@ -60,26 +64,63 @@ todoList = do
   tmplPage :: [TemplateXComponent] -> T.TemplateXElement H.Value
   tmplPage body =
     let seqBody = map (withReader f) body
-    in T.tag
+     in T.tag
+          "html"
+          (T.sattributes [])
+          [ T.stag
+              "head"
+              (H.attrlist [])
+              [ H.tag "title" (H.attrlist []) [H.text "Hyperserver"]
+              , H.script (H.attrlist [("src", "https://unpkg.com/htmx.org@1.9.4/dist/htmx.min.js")])
+              , H.link (H.attrlist [("href", "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css")])
+              ]
+          , T.tag "body" (T.sattributes []) seqBody
+          ]
+   where
+    f dict =
+      let mkeyword = lookup "keyword" dict
+          mtodoItems = getMulti "todoItems" dict
+       in ToDoModel (fromJust mkeyword) mtodoItems
+
+-- tmplToDoComponent :: TemplateXComponent
+-- tmplToDoComponent = asks todoComponent
+-- If the signature of todoComponent were (ToDoModel -> H.Html H.Value)
+--  then (asks todoComponent) :: TemplateX ToDoModel (H.Html H.Value)
+-- Refer to the actual implementation in ToDoComponent.hs
+
+search :: Applet
+search = do
+  mbody <- asks body
+  let mform = parse formData mbody
+      mkeyword = lookup "keyword" =<< mform
+      matches = maybe [] (\keyword -> filter (\slogan -> keyword `isPrefixOf` slogan) slogans) mkeyword
+      history = (`quicksearch` []) <$> matches
+      qs = quicksearch (fromMaybe "" mkeyword) matches
+      qsContainer = searchHistory $ qs : history
+      html = tmplPage [bind f qsContainer]
+      content = toStrict $ toLazyByteString (buildHtml html)
+  pure $ htmlText 200 content
+ where
+  tmplPage body =
+    H.tag
       "html"
-      (T.sattributes [])
-      [ T.stag
+      (H.attrlist [])
+      [ H.tag
           "head"
           (H.attrlist [])
           [ H.tag "title" (H.attrlist []) [H.text "Hyperserver"]
           , H.script (H.attrlist [("src", "https://unpkg.com/htmx.org@1.9.4/dist/htmx.min.js")])
           , H.link (H.attrlist [("href", "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css")])
           ]
-      , T.tag "body" (T.sattributes []) seqBody
+      , H.tag "body" (H.attrlist []) body
       ]
-    where
-      f dict =
-        let mkeyword = lookup "keyword" dict
-            mtodoItems = getMulti "todoItems" dict
-        in ToDoModel (fromJust mkeyword) mtodoItems
 
-  -- tmplToDoComponent :: TemplateXComponent
-  -- tmplToDoComponent = asks todoComponent
-  -- If the signature of todoComponent were (ToDoModel -> H.Html H.Value)
-  --  then (asks todoComponent) :: TemplateX ToDoModel (H.Html H.Value)
-  -- Refer to the actual implementation in ToDoComponent.hs
+  f (Model keyword _) = H.text keyword
+
+  slogans =
+    [ "Make.America.Great.Always"
+    , "Made.in.Germany"
+    , "Written.once.debug.everywhere"
+    , "Safe.and.effective"
+    , "Egal.was.meine.Waehler.denken"
+    ]
--- htmx-lib/Components/QuickSearch.hs
+++ htmx-lib/Components/QuickSearch.hs
@@ -1,6 +1,13 @@
-module Components.QuickSearch where
+module Components.QuickSearch (
+  QSModel (..),
+  QSAttr (..),
+  bind,
+  quicksearch,
+  searchHistory,
+) where
 
-import Data.Syntax (AttrList (AttrList), Html, Value, attr)
+import Data.Syntax (AttrList (AttrList), ChildList (ChildList), Html (SCTag, Tag, TextNode), Value)
+import qualified Data.Syntax as H
 import qualified Data.Tags as H
 
 type Keyword = String
@@ -11,12 +18,27 @@ data QSAttr
   = Delay Int
   | Rebounce Int
 
-quicksearch :: QSModel -> Html Value
-quicksearch (Model keyword results) =
-  H.div
-    (AttrList [])
-    [ H.input (AttrList [attr "value" keyword])
-    , H.ul
-        (AttrList [])
-        [H.li (AttrList []) [H.text item] | item <- results]
-    ]
+bind :: (QSModel -> Html Value) -> Html QSModel -> Html Value
+bind f = \case
+  TextNode (Model keyword results) -> renderNode keyword results
+  SCTag _ _ -> error "QS Error"
+  Tag tagName _ (ChildList children) ->
+    let c = bind f <$> children
+     in Tag tagName (H.attrlist []) $ ChildList c
+ where
+  renderNode keyword results =
+    H.form
+      (H.attrlist [("method", "POST"), ("action", "/search")])
+      [ H.input (H.attrlist [("value", keyword), ("name", "keyword")])
+      , H.input (H.attrlist [("type", "submit")])
+      , H.ul
+          (AttrList [])
+          [H.li (AttrList []) [H.text item] | item <- results]
+      ]
+
+quicksearch :: Keyword -> Results -> Html QSModel
+quicksearch keyword results = H.text $ Model keyword results
+
+searchHistory :: [Html QSModel] -> Html QSModel
+searchHistory =
+  H.tag "search-history" (H.attrlist [])
